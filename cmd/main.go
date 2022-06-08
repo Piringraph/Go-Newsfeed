c9acad9269cfe20b57c6d17bb9ee3f88bc6076a2
Pirin <chuckdanorris123@gmail.com>
2022-06-07 23:50:29 -0400

    added sqlid and content

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,21 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+/*
+
+CREATE TABLE IF NOT EXISTS "table-newsfeed" (
+	"id"	INTEGER NOT NULL,
+	"content"	TEXT,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+*/
+
 func main() {
 	db, err := sql.Open("sqlite3", "./newsfeed.db")
+
+	db.Prepare ("CREATE TABLE IF NOT EXISTS "table-newsfeed" (
+		"id"	INTEGER NOT NULL,
+		"content"	TEXT,
+		PRIMARY KEY("id" AUTOINCREMENT)
+	);")
 }
